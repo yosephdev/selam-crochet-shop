908bbb7f16494363a771b8f1dc3213db4891c087
isakbet <yosephbet@gmail.com>
2022-03-18 09:57:33 +0100

    add products object array

---
+++ products/crochets.js
@@ -0,0 +1,56 @@
+export const crochets = [
+  {
+    id: 1,
+    name: "Mittens",
+    image: "../assets/mittens.jpg",
+    description: "A warm and cozy pair of mittens!",
+    category: "adults",
+    price: 15,
+  },
+
+  {
+    id: 2,
+    name: "Hat",
+    image: "../assets/hat.jpg",
+    description: "A warm and cozy crochet beanie.",
+    category: "adults",
+    price: 20,
+  },
+
+  {
+    id: 3,
+    name: "Headband",
+    image: "../assets/headband.jpg",
+    description: "A stylish and warm headband.",
+    category: "accessories",
+    price: 10,
+  },
+
+  {
+    id: 4,
+    name: "Poncho",
+    image: "../assets/poncho.jpg",
+    description:
+      "",
+    category: "kids",
+    price: 15,
+  },
+
+  {
+    id: 5,
+    name: "Scarf",
+    image: "../assets/scarf.jpg",
+    description: "A long and luxurious crochet scarf!",
+    category: "adults",
+    price: 20,
+  },
+
+  {
+    id: 6,
+    name: "Necktie",
+    image: "../assets/necktie.jpg",
+    description: "A charming and warm oversized sweater.",
+    category: "accessories",
+    price: 10,
+  },
+];
