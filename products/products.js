bfa63cd2ab0f6ae3831b9d19f57b319f1e2c84e1
isakbet <yosephbet@gmail.com>
2022-03-19 18:47:49 +0100

    loop through and display garments using render fun

--- products/products.js
+++ products/products.js
@@ -1,15 +1,9 @@
+import { crochets } from "./crochets.js";
 import { renderCrochets } from "./crochets-render.js";
-import { updateCartCount } from "../js/utils.js";
-import { getAndSeedProducts } from "../admin/products-api.js";
 
-updateCartCount();
-
-const crochets = getAndSeedProducts();
 const productsUl = document.getElementById("product-list");
 
 for (const crochet of crochets) {
   const newCrochet = renderCrochets(crochet);
   productsUl.append(newCrochet);
 }
-
- 
